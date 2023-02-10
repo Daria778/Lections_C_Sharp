c3dd1aa997a935d949dc8a1fd9e3f3b9eaf163d2
Daria <shkatov.pad@gmail.com>
2023-02-10 20:49:21 +0300

    if_else

---
+++ Example005_if/Program.cs
@@ -0,0 +1,13 @@
+﻿// Fifth program
+Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "xiao")
+{
+    Console.WriteLine("Wow, is that Xiao?!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
